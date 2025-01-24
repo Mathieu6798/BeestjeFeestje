94fa6d8eb092b06e8ae06c52535fae6385af8e38
Mathieu6798 <mathieu9365@gmail.com>
2025-01-24 16:18:22 +0100

    Linkjes voor dieren toegevoegd

--- BeestjeFeestje/DbAccess/MyAnimalDbContext.cs
+++ BeestjeFeestje/DbAccess/MyAnimalDbContext.cs
@@ -58,22 +58,22 @@ namespace BeestjeFeestje.DbAccess
 
             // Seed data for animals
             modelBuilder.Entity<Animal>().HasData(
-                new Animal { Id = 1, Name = "Aap", Type = "Jungle", Price = 1000, Image = "https://image.parool.nl/131071330/width/2480/de-makaak-naruto-nam-honderden-foto-s-van-zichzelf-en-zijn-g" },
-                new Animal { Id = 2, Name = "Olifant", Type = "Jungle", Price = 1500, Image = "link" },
-                new Animal { Id = 3, Name = "Zebra", Type = "Jungle", Price = 2000, Image = "link" },
-                new Animal { Id = 4, Name = "Leeuw", Type = "Jungle", Price = 2500, Image = "link" },
-                new Animal { Id = 5, Name = "Hond", Type = "Boerderij", Price = 500, Image = "link" },
-                new Animal { Id = 6, Name = "Ezel", Type = "Boerderij", Price = 750, Image = "link" },
-                new Animal { Id = 7, Name = "Koe", Type = "Boerderij", Price = 1000, Image = "link" },
-                new Animal { Id = 8, Name = "Eend", Type = "Boerderij", Price = 1250, Image = "link" },
-                new Animal { Id = 9, Name = "Kuiken", Type = "Boerderij", Price = 1500, Image = "link" },
-                new Animal { Id = 10, Name = "Pinguïn", Type = "Sneeuw", Price = 750, Image = "link" },
-                new Animal { Id = 11, Name = "IJsbeer", Type = "Sneeuw", Price = 1000, Image = "link" },
-                new Animal { Id = 12, Name = "Zeehond", Type = "Sneeuw", Price = 1250, Image = "link" },
-                new Animal { Id = 13, Name = "Kameel", Type = "Woestijn", Price = 750, Image = "link" },
-                new Animal { Id = 14, Name = "Slang", Type = "Woestijn", Price = 1000, Image = "link" },
-                new Animal { Id = 15, Name = "T-Rex", Type = "VIP", Price = 5000, Image = "link" },
-                new Animal { Id = 16, Name = "Unicorn", Type = "VIP", Price = 10000, Image = "link" }
+                new Animal { Id = 1, Name = "Aap", Type = "Jungle", Price = 1000, Image = "https://img.freepik.com/free-photo/cute-long-tailed-macaque-eating-fruits-mauritius_181624-47645.jpg?semt=ais_hybrid" },
+                new Animal { Id = 2, Name = "Olifant", Type = "Jungle", Price = 1500, Image = "https://img.freepik.com/free-photo/beautiful-shot-african-elephant-savanna-field_181624-13908.jpg?semt=ais_hybrid" },
+                new Animal { Id = 3, Name = "Zebra", Type = "Jungle", Price = 2000, Image = "https://img.freepik.com/free-photo/beautiful-baby-zebra-sitting-ground-captured-african-jungle_181624-35400.jpg?semt=ais_hybrid" },
+                new Animal { Id = 4, Name = "Leeuw", Type = "Jungle", Price = 2500, Image = "https://img.freepik.com/free-photo/female-lion-animal-orphanage-kenya_181624-47473.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 5, Name = "Hond", Type = "Boerderij", Price = 500, Image = "https://img.freepik.com/free-photo/closeup-shot-black-labrador-playing-grass-surrounded-by-greenery_181624-24383.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 6, Name = "Ezel", Type = "Boerderij", Price = 750, Image = "https://img.freepik.com/free-photo/very-sweet-solitary-burro-standing-grass-meadow_493961-1064.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 7, Name = "Koe", Type = "Boerderij", Price = 1000, Image = "https://img.freepik.com/free-photo/cows-standing-green-field-front-fuji-mountain-japan_335224-197.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 8, Name = "Eend", Type = "Boerderij", Price = 1250, Image = "https://img.freepik.com/premium-photo/duck-drake-green-grass_167689-1415.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 9, Name = "Kuiken", Type = "Boerderij", Price = 1500, Image = "https://img.freepik.com/premium-photo/newborn-little-cute-yellow-duckling-isolated-white_174533-1086.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 10, Name = "Pinguïn", Type = "Sneeuw", Price = 750, Image = "https://img.freepik.com/free-photo/penguin-walking-frozen-beach_181624-50490.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 11, Name = "IJsbeer", Type = "Sneeuw", Price = 1000, Image = "https://img.freepik.com/premium-photo/polar-bear-ursus-maritimus-cub-pack-ice_160321-4130.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 12, Name = "Zeehond", Type = "Sneeuw", Price = 1250, Image = "https://img.freepik.com/free-photo/seal-beach-dune-island-near-helgoland_475641-186.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 13, Name = "Kameel", Type = "Woestijn", Price = 750, Image = "https://img.freepik.com/free-photo/dromedary-portrait_463209-91.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 14, Name = "Slang", Type = "Woestijn", Price = 1000, Image = "https://img.freepik.com/free-photo/green-viper-snake-branch_488145-411.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 15, Name = "T-Rex", Type = "VIP", Price = 5000, Image = "https://img.freepik.com/premium-photo/tyrannosaurus-t-rex-dinosaur-smoke-background_44074-5290.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" },
+                new Animal { Id = 16, Name = "Unicorn", Type = "VIP", Price = 10000, Image = "https://img.freepik.com/free-photo/photorealistic-unicorn-creature_23-2151581099.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid" }
             );
         }
     }
---
+++ BeestjeFeestje/Migrations/20250124151809_First.cs
@@ -0,0 +1,362 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BeestjeFeestje.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Animals",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Price = table.Column<int>(type: "int", nullable: false),
+                    Image = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Animals", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoles",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUsers",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerCard = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedUserName = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    Email = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    NormalizedEmail = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: true),
+                    EmailConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SecurityStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConcurrencyStamp = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(type: "bit", nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    LockoutEnabled = table.Column<bool>(type: "bit", nullable: false),
+                    AccessFailedCount = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUsers", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetRoleClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetRoleClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetRoleClaims_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserClaims",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ClaimType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClaimValue = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserClaims", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserClaims_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserLogins",
+                columns: table => new
+                {
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderKey = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ProviderDisplayName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserLogins", x => new { x.LoginProvider, x.ProviderKey });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserLogins_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserRoles",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    RoleId = table.Column<string>(type: "nvarchar(450)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserRoles", x => new { x.UserId, x.RoleId });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetRoles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "AspNetRoles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AspNetUserRoles_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AspNetUserTokens",
+                columns: table => new
+                {
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    LoginProvider = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AspNetUserTokens", x => new { x.UserId, x.LoginProvider, x.Name });
+                    table.ForeignKey(
+                        name: "FK_AspNetUserTokens_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bookings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    BookedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bookings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Bookings_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookingAnimals",
+                columns: table => new
+                {
+                    BookingId = table.Column<int>(type: "int", nullable: false),
+                    AnimalId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookingAnimals", x => new { x.BookingId, x.AnimalId });
+                    table.ForeignKey(
+                        name: "FK_BookingAnimals_Animals_AnimalId",
+                        column: x => x.AnimalId,
+                        principalTable: "Animals",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookingAnimals_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    BookingId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Contacts_Bookings_BookingId",
+                        column: x => x.BookingId,
+                        principalTable: "Bookings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "Id", "Image", "Name", "Price", "Type" },
+                values: new object[,]
+                {
+                    { 1, "https://img.freepik.com/free-photo/cute-long-tailed-macaque-eating-fruits-mauritius_181624-47645.jpg?semt=ais_hybrid", "Aap", 1000, "Jungle" },
+                    { 2, "https://img.freepik.com/free-photo/beautiful-shot-african-elephant-savanna-field_181624-13908.jpg?semt=ais_hybrid", "Olifant", 1500, "Jungle" },
+                    { 3, "https://img.freepik.com/free-photo/beautiful-baby-zebra-sitting-ground-captured-african-jungle_181624-35400.jpg?semt=ais_hybrid", "Zebra", 2000, "Jungle" },
+                    { 4, "https://img.freepik.com/free-photo/female-lion-animal-orphanage-kenya_181624-47473.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Leeuw", 2500, "Jungle" },
+                    { 5, "https://img.freepik.com/free-photo/closeup-shot-black-labrador-playing-grass-surrounded-by-greenery_181624-24383.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Hond", 500, "Boerderij" },
+                    { 6, "https://img.freepik.com/free-photo/very-sweet-solitary-burro-standing-grass-meadow_493961-1064.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Ezel", 750, "Boerderij" },
+                    { 7, "https://img.freepik.com/free-photo/cows-standing-green-field-front-fuji-mountain-japan_335224-197.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Koe", 1000, "Boerderij" },
+                    { 8, "https://img.freepik.com/premium-photo/duck-drake-green-grass_167689-1415.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Eend", 1250, "Boerderij" },
+                    { 9, "https://img.freepik.com/premium-photo/newborn-little-cute-yellow-duckling-isolated-white_174533-1086.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Kuiken", 1500, "Boerderij" },
+                    { 10, "https://img.freepik.com/free-photo/penguin-walking-frozen-beach_181624-50490.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Pinguïn", 750, "Sneeuw" },
+                    { 11, "https://img.freepik.com/premium-photo/polar-bear-ursus-maritimus-cub-pack-ice_160321-4130.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "IJsbeer", 1000, "Sneeuw" },
+                    { 12, "https://img.freepik.com/free-photo/seal-beach-dune-island-near-helgoland_475641-186.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Zeehond", 1250, "Sneeuw" },
+                    { 13, "https://img.freepik.com/free-photo/dromedary-portrait_463209-91.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Kameel", 750, "Woestijn" },
+                    { 14, "https://img.freepik.com/free-photo/green-viper-snake-branch_488145-411.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Slang", 1000, "Woestijn" },
+                    { 15, "https://img.freepik.com/premium-photo/tyrannosaurus-t-rex-dinosaur-smoke-background_44074-5290.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "T-Rex", 5000, "VIP" },
+                    { 16, "https://img.freepik.com/free-photo/photorealistic-unicorn-creature_23-2151581099.jpg?ga=GA1.1.1551102030.1737730470&semt=ais_hybrid", "Unicorn", 10000, "VIP" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetRoleClaims_RoleId",
+                table: "AspNetRoleClaims",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "RoleNameIndex",
+                table: "AspNetRoles",
+                column: "NormalizedName",
+                unique: true,
+                filter: "[NormalizedName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserClaims_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserLogins_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUserRoles_RoleId",
+                table: "AspNetUserRoles",
+                column: "RoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "EmailIndex",
+                table: "AspNetUsers",
+                column: "NormalizedEmail");
+
+            migrationBuilder.CreateIndex(
+                name: "UserNameIndex",
+                table: "AspNetUsers",
+                column: "NormalizedUserName",
+                unique: true,
+                filter: "[NormalizedUserName] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookingAnimals_AnimalId",
+                table: "BookingAnimals",
+                column: "AnimalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_UserId",
+                table: "Bookings",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_BookingId",
+                table: "Contacts",
+                column: "BookingId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AspNetRoleClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserClaims");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserLogins");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserRoles");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUserTokens");
+
+            migrationBuilder.DropTable(
+                name: "BookingAnimals");
+
+            migrationBuilder.DropTable(
+                name: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "AspNetRoles");
+
+            migrationBuilder.DropTable(
+                name: "Animals");
+
+            migrationBuilder.DropTable(
+                name: "Bookings");
+
+            migrationBuilder.DropTable(
+                name: "AspNetUsers");
+        }
+    }
+}
